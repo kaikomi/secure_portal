d8d511b7e4d929988d6924c99c1d760c58d69532
cheesypasta <rozaphal123@gmail.com>
2026-06-23 16:51:08 +0700

    change

--- database.sql
+++ database.sql
@@ -26,5 +26,5 @@ CREATE TABLE IF NOT EXISTS users (
 INSERT INTO users (username, email, password) VALUES (
     'testuser',
     'test@example.com',
-    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+    '$2y$10$tam8sOFTbcwuS5CuGvqTq.ExM6OJJlXn289lKmLsg0YWHRRxS7nm2'
 );
